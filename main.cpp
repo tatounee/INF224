ec8e40021beaa247dea2814959e84c6b7a4c5c49
Tatounee <66035819+Tatounee@users.noreply.github.com>
2024-12-17 15:02:55 +0100

    Exemple d'utilisation de Group

--- main.cpp
+++ main.cpp
@@ -1,28 +1,27 @@
 
 #include <iostream>
 
-#include "movie.h"
+#include "group.h"
 
 int main()
 {
-    uint32_t chapters[] = {0, 10, 17, 23};
+    Media *m1 = new Media("1mm1", "1pp1");
+    Media *m2 = new Media("2mm2", "2pp2");
+    Media *m3 = new Media("3mm3", "3pp3");
+    Media *m4 = new Media("4mm4", "4pp4");
 
-    Movie *mov = new Movie(
-        std::string("TMS"),
-        std::string("/home/tatoune/Videos/Screencasts/tsm.mp4"),
-        35,
-        chapters,
-        4);
+    Group *g1 = new Group("g1");
+    g1->push_back(m1);
+    g1->push_back(m2);
+    g1->push_back(m3);
 
-    mov->display(std::cout);
+    Group *g2 = new Group("g2");
+    g2->push_back(m1);
+    g2->push_back(m4);
 
-    uint32_t *newChapters = new uint32_t[2]{42, 666};
-
-    mov->setChapters(newChapters, 2);
-    mov->display(std::cout);
-
-    delete[] newChapters;
-    mov->display(std::cout);
+    g1->display(std::cout);
+    std::cout << "---------------------" << std::endl;
+    g2->display(std::cout);
 
     return 0;
 }
