2ea5b6807b7cbf06468a6da82c8f305625854992
Tatounee <66035819+Tatounee@users.noreply.github.com>
2025-01-29 22:26:49 +0100

    Documentation de Group

--- group.h
+++ group.h
@@ -6,18 +6,24 @@
 
 #include "media.h"
 
+/// @brief Représente un groupe de médias.
 class Group : public std::list<Media *>
 {
 private:
     std::string name{};
 
 public:
+    /// @brief Constructeur par défaut.
     Group() : name(std::string()) {};
+
+    /// @brief Constructeur avec paramètres.
+    /// @param name Le nom du groupe.
     Group(std::string name) : name{name} {};
 
     std::string getName() const { return this->name; };
     void setName(std::string name) { this->name = name; };
 
+    /// @brief Affiche les informations du groupe.
     void display(std::ostream &sout) const
     {
         sout << "[Group]" << std::endl
