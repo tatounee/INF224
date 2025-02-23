1779de0530f2cb691644a720a7c4ac7e042d16ef
Tatounee <66035819+Tatounee@users.noreply.github.com>
2025-02-23 15:39:48 +0100

    JAVA: Etape 2

--- Delfour__Matteo/swing/Main.java
+++ Delfour__Matteo/swing/Main.java
@@ -1,46 +1,66 @@
 package swing;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Main extends JFrame {
-    JScrollPane scrollPane;
-    JTextArea text;
+    // JScrollPane scrollPane;
+    // JTextArea text;
 
-    JPanel buttonPanel;
-    JButton button0;
-    JButton button1;
-    JButton button2;
-
-    public static void main(String argv[]) {
-        Main toplevel = new Main();
-    }
+    // JMenuBar menuBar;
+    // JToolBar toolBar;
+    // Button button0;
+    // Button button1;
+    // Button button2;
 
     public Main() {
-        text = new JTextArea(10, 30);
+        JTextArea text = new JTextArea(10, 30);
         text.setLineWrap(true);
         text.setWrapStyleWord(true);
-        scrollPane = new JScrollPane(text);
+        JScrollPane scrollPane = new JScrollPane(text);
 
-        buttonPanel = new JPanel();
-        button0 = new JButton("Click 0");
-        button1 = new JButton("Click 1");
-        button2 = new JButton("Quit");
+        Button button0 = new Button("Click 0", (event) -> text.append("Button 0 clicked\n"));
+        Button button1 = new Button("Click 1", (event) -> text.append("Button 1 clicked\n"));
+        Button button2 = new Button("Quit", (event) -> System.exit(0));
 
-        button0.addActionListener((event) -> text.append("Button 0 clicked\n"));
-        button1.addActionListener((event) -> text.append("Button 1 clicked\n"));
-        button2.addActionListener((event) -> System.exit(0));
+        JToolBar toolBar = new JToolBar();
+        toolBar.add(button0);
+        toolBar.add(button1);
+        toolBar.add(button2);
 
-        buttonPanel.add(button0);
-        buttonPanel.add(button1);
-        buttonPanel.add(button2);
+        JMenu menu = new JMenu("Action");
+        menu.add(new JMenuItem(button0));
+        menu.add(new JMenuItem(button1));
+        menu.add(new JMenuItem(button2));
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(menu);
 
+        setJMenuBar(menuBar);
         getContentPane().add(scrollPane, "Center");
-        getContentPane().add(buttonPanel, "South");
+        getContentPane().add(toolBar, "South");
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setTitle("Télécommande");
         pack();
         setVisible(true);
+    }
+
+    public static void main(String argv[]) {
+        Main toplevel = new Main();
+    }
+
+}
+
+class Button extends AbstractAction {
+    ActionListener action;
+
+    public Button(String name, ActionListener actionListener) {
+        super(name);
+        action = actionListener;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        action.actionPerformed(e);
     }
 }
