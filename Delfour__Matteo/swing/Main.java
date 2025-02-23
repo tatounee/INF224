6f7f20190e521241b599df104af0d8c03ce5196d
Tatounee <66035819+Tatounee@users.noreply.github.com>
2025-02-23 15:19:25 +0100

    JAVA: Etape1

--- Delfour__Matteo/swing/Main.java
+++ Delfour__Matteo/swing/Main.java
@@ -2,33 +2,45 @@ package swing;
 
 import javax.swing.*;
 
-public class Main {
-    /**
-     * Create the GUI and show it. For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private static void createAndShowGUI() {
-        // Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class Main extends JFrame {
+    JScrollPane scrollPane;
+    JTextArea text;
 
-        // Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
+    JPanel buttonPanel;
+    JButton button0;
+    JButton button1;
+    JButton button2;
 
-        // Display the window.
-        frame.pack();
-        frame.setVisible(true);
+    public static void main(String argv[]) {
+        Main toplevel = new Main();
     }
 
-    public static void main(String[] args) {
-        // Schedule a job for the event-dispatching thread:
-        // creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+    public Main() {
+        text = new JTextArea(10, 30);
+        text.setLineWrap(true);
+        text.setWrapStyleWord(true);
+        scrollPane = new JScrollPane(text);
+
+        buttonPanel = new JPanel();
+        button0 = new JButton("Click 0");
+        button1 = new JButton("Click 1");
+        button2 = new JButton("Quit");
+
+        button0.addActionListener((event) -> text.append("Button 0 clicked\n"));
+        button1.addActionListener((event) -> text.append("Button 1 clicked\n"));
+        button2.addActionListener((event) -> System.exit(0));
+
+        buttonPanel.add(button0);
+        buttonPanel.add(button1);
+        buttonPanel.add(button2);
+
+        getContentPane().add(scrollPane, "Center");
+        getContentPane().add(buttonPanel, "South");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setTitle("Télécommande");
+        pack();
+        setVisible(true);
     }
 }
